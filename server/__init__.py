2761923e5fac2529bb4c9d4d32f3be3120536202
rbaudu <3866570+rbaudu@users.noreply.github.com>
2025-03-15 12:17:09 +0100

    Ajout du fichier d'initialisation server/__init__.py pour résoudre les problèmes d'importation

--- server/__init__.py
+++ server/__init__.py
@@ -1,1 +1,39 @@
-# Ce fichier indique que le répertoire server est un package Python
+# Fichier d'initialisation du package server
+# Cela permet d'importer les modules du package server
+
+# Exporter les configurations essentielles pour une utilisation facile
+from server.config import Config
+
+# Configuration OBS exportée au niveau du module
+OBS_HOST = Config.OBS_HOST
+OBS_PORT = Config.OBS_PORT
+OBS_PASSWORD = Config.OBS_PASSWORD
+
+# Autres configurations essentielles
+WEB_PORT = Config.FLASK_PORT
+ANALYSIS_INTERVAL = Config.ANALYSIS_INTERVAL
+
+# Variables pour l'interface
+ACTIVITY_ICONS = {
+    'endormi': 'moon',
+    'à table': 'utensils',
+    'lisant': 'book-open',
+    'au téléphone': 'phone',
+    'en conversation': 'users',
+    'occupé': 'briefcase',
+    'inactif': 'pause-circle'
+}
+
+ACTIVITY_COLORS = {
+    'endormi': 'indigo',
+    'à table': 'orange',
+    'lisant': 'teal',
+    'au téléphone': 'purple',
+    'en conversation': 'blue',
+    'occupé': 'red',
+    'inactif': 'gray'
+}
+
+# Variables pour la capture
+VIDEO_SOURCE_NAME = 'Default Video Source'
+AUDIO_SOURCE_NAME = 'Default Audio Source'
